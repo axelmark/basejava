61615e8c3368522d62e14e0808d8317128001ca0
axelmark <axelmark@mail.ru>
2021-03-01 19:45:34 +0300

    Commit

--- src/MainArray.java
+++ src/MainArray.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.UUID;
 
 /**
  *
@@ -35,7 +36,7 @@ public class MainArray {
                     break;
                 case "save":
                     r = new Resume();
-                    r.uuid = uuid;
+                    r.uuid = UUID.randomUUID().toString();
                     ARRAY_STORAGE.save(r);
                     printAll();
                     break;
